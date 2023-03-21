2e01412ac70493b4a1467e5398ec6f545da7ddb7
fl-hi1 <73307425+fl-hi1@users.noreply.github.com>
2023-03-21 22:33:37 +0100

    switched gamma and delta with gamma_inv and delta_inv in the SA

--- local_SEIR.py
+++ local_SEIR.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 def solveSEIR(input_parameters):
     
     #for odeint
-    def seir_model(y, t, N, beta, alpha, gamma, delta, kappa, lam):
+    def seir_model(y, t, N, beta, alpha, gamma_inv, delta_inv, kappa, lam):
         S, E, I, Q, R, D, P = y
         dSdt = -beta*S*I/N - alpha*S
-        dEdt = beta*S*I/N - gamma*E
-        dIdt = gamma*E - delta*I
-        dQdt = delta*I - lam*Q - kappa*Q
+        dEdt = beta*S*I/N - 1/(gamma_inv)*E
+        dIdt = 1/(gamma_inv)*E - 1/(delta_inv)*I
+        dQdt = 1/(delta_inv)*I - lam*Q - kappa*Q
         dRdt = lam*Q
         dDdt = kappa*Q
         dPdt = alpha*S
@@ -43,17 +43,18 @@ def solveSEIR(input_parameters):
     S0 = N - E0 - I0 - Q0 - R0 - D0 - P0
     
     # Parameters must be in the same order 
-    beta,alpha,gamma,delta,kappa,lam = input_parameters[0]
+    beta,alpha,gamma_inv,delta_inv,kappa,lam = input_parameters[0]
+
 
     # Set time discretization as in the paper
     t = np.linspace(0, 180, 10000)
 
     # Solve system of differential equations
     # Returns the seven variables S, E, I, Q, R, D, and P at each time point
-    sol = odeint(seir_model, [S0, E0, I0, Q0, R0, D0, P0], t, args=(N, beta, alpha, gamma, delta, kappa, lam))
+    sol = odeint(seir_model, [S0, E0, I0, Q0, R0, D0, P0], t, args=(N, beta, alpha, gamma_inv, delta_inv, kappa, lam))
 
     
     # dSdt, dEdt, dIdt, dQdt, dRdt, dDdt, dPdt
 
-    # returns susceptible people
-    return sol.T[0]
+    # returns infected people
+    return sol.T[2]

540fc188c295fed0af1c869304d814a7ac1c1452
teobucci <teobucci8@gmail.com>
2023-03-22 12:29:04 +0100

    Lots of stuff

--- local_SEIR.py
+++ local_SEIR.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import numpy as np
 import matplotlib as plt
@@ -7,10 +6,58 @@ import matplotlib.pyplot as plt
 
 #MODIFIED VERSION WITH ONE OUTPUT
 
-def solveSEIR(input_parameters):
+def solveSEIR_UQpy(input_parameters):
     
     # for odeint
-    def seir_model(y, t, N, beta, alpha, gamma_inv, delta_inv, kappa, lam):
+    def seir_model(y, t, N, alpha, beta, gamma_inv, delta_inv, lam, kappa):
+        S, E, I, Q, R, D, P = y
+        dSdt = -beta*S*I/N - alpha*S
+        dEdt = beta*S*I/N - 1/gamma_inv*E
+        dIdt = 1/gamma_inv*E - 1/delta_inv*I
+        dQdt = 1/delta_inv*I - lam*Q - kappa*Q
+        dRdt = lam*Q
+        dDdt = kappa*Q
+        dPdt = alpha*S
+        
+        return dSdt, dEdt, dIdt, dQdt, dRdt, dDdt, dPdt
+
+    # Initial conditions
+    N = 14000000
+    E0 = 318
+    I0 = 389
+    Q0 = 700
+    R0 = 0
+    D0 = 0
+    P0 = 0
+
+    S0 = N - E0 - I0 - Q0 - R0 - D0 - P0
+    
+    # unpack the input parameters
+    # Parameters must be in the same order 
+    alpha, beta, gamma_inv, delta_inv, lam, kappa = input_parameters[0]
+
+
+    # Set time discretization as in the paper
+    t = np.linspace(0, 180, 10000)
+
+    # Solve system of differential equations
+    # Returns the seven variables S, E, I, Q, R, D, and P at each time point
+    sol = odeint(func=seir_model,
+                 y0=[S0, E0, I0, Q0, R0, D0, P0],
+                 t=t,
+                 args=(N, alpha, beta, gamma_inv, delta_inv, lam, kappa))
+
+    
+    # dSdt, dEdt, dIdt, dQdt, dRdt, dDdt, dPdt
+
+    # returns infected people
+    return sol.T[2]
+
+
+def solveSEIR(input_parameters):
+
+    # for odeint
+    def seir_model(y, t, N, alpha, beta, gamma_inv, delta_inv, lam, kappa):
         S, E, I, Q, R, D, P = y
         dSdt = -beta*S*I/N - alpha*S
         dEdt = beta*S*I/N - 1/gamma_inv*E
@@ -34,7 +81,7 @@ def solveSEIR(input_parameters):
     S0 = N - E0 - I0 - Q0 - R0 - D0 - P0
     
     # Parameters must be in the same order 
-    beta, alpha, gamma_inv, delta_inv, kappa, lam = input_parameters[0]
+    alpha, beta, gamma_inv, delta_inv, lam, kappa = input_parameters
 
 
     # Set time discretization as in the paper
@@ -45,10 +92,6 @@ def solveSEIR(input_parameters):
     sol = odeint(func=seir_model,
                  y0=[S0, E0, I0, Q0, R0, D0, P0],
                  t=t,
-                 args=(N, beta, alpha, gamma_inv, delta_inv, kappa, lam))
+                 args=(N, alpha, beta, gamma_inv, delta_inv, lam, kappa))
 
-    
-    # dSdt, dEdt, dIdt, dQdt, dRdt, dDdt, dPdt
-
-    # returns infected people
-    return sol.T[2]
+    return sol.T
